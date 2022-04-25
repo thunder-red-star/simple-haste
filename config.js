b73b5d61c9e01fcd8e9a6996aa477002531e8285
ThunderRedStar <thunderredstar@thunderredstar.tech>
2022-04-25 13:57:06 -0400

    home link

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
 	maxHasteLength: 1_000_000,
 	port: 3000,
 	templateDir: path.resolve(`${process.cwd()}${path.sep}views`),
-	name: "TH"
+	name: "TH",
+	baseUrl: "https://haste.thdr.me"
 }
---
+++ endpoints/create-form.js
@@ -0,0 +1,29 @@
+const Utils = require('../utils/utils');
+const config = require('../config');
+
+module.exports = {
+    data: {
+        method: 'POST',
+        name: 'create',
+        description: 'Create a new haste entry',
+    },
+    async execute (req, res) {
+        // Get name and content from request body
+        const { name, description, content } = req.body;
+
+        // Create new entry
+        const entry = await Utils.createNewHaste(name, description, content);
+
+        // Check if successful
+        if (entry.success) {
+            // Send success response
+            res.redirect(`${config.baseUrl}/code/${entry.id}`);
+        } else {
+            // Send error response
+            res.status(500).json({
+                success: false,
+                message: 'Error creating entry: ' + entry.error,
+            });
+        }
+    }
+}
