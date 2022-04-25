ae408141980ea2e4b5362ef586053bd2cf3bbd9b
ThunderRedStar <thunderredstar@thunderredstar.tech>
2022-04-25 14:03:11 -0400

    formatting

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
 	maxHasteLength: 1_000_000,
 	port: 3000,
 	templateDir: path.resolve(`${process.cwd()}${path.sep}views`),
-	name: "TH",
+	name: "ThunderHaste",
 	baseUrl: "https://haste.thdr.me"
 }
